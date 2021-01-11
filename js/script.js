241fa2f4dbb0122f04d4aba822cea61d8fcd9113
Aleksey <sett44@gmail.com>
2021-01-11 16:21:16 +0300

    1st commit

---
+++ js/script.js
@@ -0,0 +1,23 @@
+"use strict"; //строго в начале скрипта
+
+console.log('arr'+"-object");
+console.log(4+ "5");
+
+let incr=10,
+    decr=10;
+//++incr; //форма записи префиксной это до переменной, постфиксной когда после
+//--decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4===8);
+
+
+const isChecked=true,
+      isClose=true;
+console.log(isChecked&&isClose);
+
+
